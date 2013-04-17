95284310d496412613576769437ac65ef3f5eb6e
wpapsco <wpapsco@gmail.com>
2013-04-17 12:55:29 -0700

    commit

--- AStarSearch/src/State.java
+++ AStarSearch/src/State.java
@@ -3,11 +3,13 @@ import java.util.ArrayList;
 public abstract class State {
 	private ArrayList<Node> path;
 	public int computeCost() {
+		int val = 0;
 		for (int i = 0; i < path.size(); i++) {
 			for (int j = 0; j < path.get(i).getChildNodeCosts().size(); j++) {
-				
+				val = val + path.get(i).getChildNodeCosts().get(j);
 			}
 		}
-		return 0;
+		val = val + path.get(path.size() - 1).getHeuristic();
+		return val;
 	}
 }
